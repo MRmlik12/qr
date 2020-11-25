bd64c37ecff02f15598933e66c350cad1145b1d3
BuildTools <unconfigured@null.spigotmc.org>
2020-11-25 08:21:30 -0100

    Add .NET implementation

---
+++ dotnet/WulkanowyQr.Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WulkanowyQr;
+
+namespace WulkanowyQr.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encode = Qr.Encode("ABCDEFG", "CODE");
+            var decode = Qr.Decode("ABCDEFG", "ENCRYPTED_CODE");
+        }
+    }
+}
